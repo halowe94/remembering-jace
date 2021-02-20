c148d93a8880b5a0d83e012a466a96737ba9816e
halowe <haleylowe0@gmail.com>
2021-02-19 20:47:21 -0800

    made footer into sticky footer via css

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
     <div>
-      <footer class="footer">
+      <footer class="footer" id="footer">
         <div class="two-toned-footer-color"></div>
         <p class="text-muted text-muted-footer text-center">&copy; Copyright</p>
       </footer>
