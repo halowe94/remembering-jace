f48e971d2a8c7fd140ed0d473b10a7011e49b04e
halowe <haleylowe0@gmail.com>
2021-02-19 19:57:31 -0800

    added html in footer component

---
+++ src/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <footer class="footer">
+        <div class="two-toned-footer-color"></div>
+        <p class="text-muted text-muted-footer text-center">&copy; Copyright</p>
+      </footer>
+    </div>
+  );
+}
