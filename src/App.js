bfb2b4c31b8dec628da61ee1a6d39c27aa0e4e0e
halowe <haleylowe0@gmail.com>
2021-02-19 20:00:30 -0800

    export default Footer, added Footer component to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./NavBar/NavBar";
+import Footer from "./Footer/Footer";
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
       <header className="App-header">
         <NavBar />
       </header>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
