8bf47df1c0a1e4875bd843674531ed972b1f4bd7
halowe <haleylowe0@gmail.com>
2021-03-08 23:45:32 -0800

    created new react app and installed dependencies

--- src/ImageBox/ImageBox.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "react-bootstrap";
-import "./ImageBox.css";
-
-function ImageBox() {
-  return <div className="ImageBox" id="ImageBox"></div>;
-}
-export default ImageBox;
