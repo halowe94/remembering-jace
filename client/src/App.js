2f9f9843a614c6a6e0408d91bd5fc9c1df0d958c
halowe <haleylowe0@gmail.com>
2021-03-09 00:42:40 -0800

    added footer component to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./NavBar/NavBar";
+import Footer from "./Footer/Footer";
 
 function App() {
-  return <NavBar />;
+  return (
+    <div>
+      <NavBar />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
