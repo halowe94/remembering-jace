e7cf034ce6576673fdd1f44847f9f57aac151dec
halowe <haleylowe0@gmail.com>
2021-03-09 00:39:44 -0800

    footer css to make sticky footer. imported to footer js page

--- client/src/Footer/Footer.js
+++ client/src/Footer/Footer.js
@@ -1,11 +1,14 @@
 import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
-    <footer class="footer">
-      <div class="two-toned-footer-color"></div>
-      <p class="text-muted text-muted-footer text-center">&copy; Copyright</p>
-    </footer>
+    <div>
+      <footer class="footer" id="footer">
+        <div class="two-toned-footer-color"></div>
+        <p class="text-muted text-muted-footer text-center">&copy; Copyright</p>
+      </footer>
+    </div>
   );
 }
 
