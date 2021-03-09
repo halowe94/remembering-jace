7652d6e265b5b2f76433189c0ca68cfada403acd
halowe <haleylowe0@gmail.com>
2021-03-09 00:34:22 -0800

    created Footer component

---
+++ client/src/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer class="footer">
+      <div class="two-toned-footer-color"></div>
+      <p class="text-muted text-muted-footer text-center">&copy; Copyright</p>
+    </footer>
+  );
+}
+
+export default Footer;
