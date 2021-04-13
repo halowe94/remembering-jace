298f220fe460e305137efc9a19e70998657963db
halowe <haleylowe0@gmail.com>
2021-04-12 23:55:05 -0700

    added text to TextDiv

--- client/src/TextDiv/TextDiv.js
+++ client/src/TextDiv/TextDiv.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function TextDiv() {
-  return <div></div>;
+  return (
+    <div>
+      <p className="aboutJ">
+        Jace Young was a 6 year old boy full of life and light. Those who knew
+        him well describe him as a child who loved to read, who was always
+        dancing, and who was incredibly smart. Jace attended Kipp Bayview and
+        had dreams of becoming a computer programmer. Today we remember his
+        sweet smile, and though he is gone too soon, there is no doubt that he
+        was a blessing to all who knew him.
+      </p>
+    </div>
+  );
 }
 
 export default TextDiv;
