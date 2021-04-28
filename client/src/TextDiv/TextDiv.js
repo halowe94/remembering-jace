23144f15f5e12608b04ee3e8dbbe68cf9963664b
halowe <haleylowe0@gmail.com>
2021-04-28 16:39:49 -0700

    added more p tags

--- client/src/TextDiv/TextDiv.js
+++ client/src/TextDiv/TextDiv.js
@@ -4,19 +4,24 @@ import "./TextDiv.css";
 function TextDiv() {
   return (
     <div>
-      <p className="aboutJ">
-        Jace Young was a 6 year old boy full of life and light. Those who knew
-        him well describe him as a child who loved to read, who was always
-        dancing, and who was incredibly smart. Jace attended Kipp Bayview and
-        had dreams of becoming a computer programmer. Today we remember his
-        sweet smile, and though he is gone too soon, there is no doubt that he
-        was a blessing to all who knew him. "And when great souls die, after a
-        period peace blooms, slowly and always irregularly. Spaces fill with a
-        kind of soothing electric vibration. Our senses, restored, never to be
-        the same, whisper to us. They existed. They existed. We can be. Be and
-        be better. For they existed. Source:
-        https://www.familyfriendpoems.com/poem/when-great-trees-fall-by-maya-angelou"
-      </p>
+      <div className="aboutJ">
+        <p>
+          Jace Young was a 6 year old boy full of life and light. Those who knew
+          him well describe him as a child who loved to read, who was always
+          dancing, and who was incredibly smart. Jace attended Kipp Bayview and
+          had dreams of becoming a computer programmer. Today we remember his
+          sweet smile, and though he is gone too soon, there is no doubt that he
+          was a blessing to all who knew him.
+        </p>
+        <p>
+          "And when great souls die, after a period peace blooms, slowly and
+          always irregularly. Spaces fill with a kind of soothing electric
+          vibration. Our senses, restored, never to be the same, whisper to us.
+          They existed. They existed. We can be. Be and be better. For they
+          existed. Source:
+          https://www.familyfriendpoems.com/poem/when-great-trees-fall-by-maya-angelou"
+        </p>
+      </div>
     </div>
   );
 }
